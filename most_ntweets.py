fa83084246d1e914cd1e76299632ba672dfac206
Hernan Larrain <hlarrainq@uc.cl>
2022-03-26 20:41:52 -0300

    top user by number of tweets

---
+++ most_ntweets.py
@@ -0,0 +1,21 @@
+import json
+
+def top_user_ntweets(tweetfile, x):
+    users = {}
+    file = open(tweetfile, 'r')
+    for line in file:
+        try:
+            tweet = json.loads(line)
+        except:
+            continue
+        user = tweet['user']['username']
+        if user not in users.keys():
+            users[user] = 1
+        else:
+            users[user] += 1
+    # convert to list
+    users = dict(sorted(users.items(), key=lambda item: item[1], reverse=True))
+    top = list(users.keys())[0:x]
+    return top
+
+top_user_ntweets('db.json', 10)
